b9dd95e030a2eda1f66f62aff87a1b86748c13b9
unknown <stolyarova.199535@gmail.com>
2023-03-03 10:35:20 +0300

    all

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,4 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-
-// import {initSearch} from './modules/init-search.js';
-
-
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -14,34 +10,84 @@ window.addEventListener('DOMContentLoaded', () => {
   // Modules
   // ---------------------------------
 
+  // Form
+  const telephones = document.querySelectorAll('input[type="tel"]');
+
+  const prefixNumber = (str) => {
+    if (str === '7') {
+      return '7 (';
+    }
+    if (str === '8') {
+      return '7 (';
+    }
+    if (str === '9') {
+      return '7 (';
+    }
+    return '7 (';
+  };
+
+  // ---------------
+  for (let i = 0; i < telephones.length; i++) {
+    telephones[i].addEventListener('input', () => {
+      const value = telephones[i].value.replace(/\D+/g, '');
+      const numberLength = 11;
+
+      let result;
+      if (telephones[i].value.includes('+8') || telephones[i].value[0] === '8') {
+        result = '';
+      } else {
+        result = '+';
+      }
+
+      //
+      for (let j = 0; j < value.length && j < numberLength; j++) {
+        switch (j) {
+          case 0:
+            result += prefixNumber(value[i]);
+            continue;
+          case 4:
+            result += ') ';
+            break;
+          case 7:
+            result += '-';
+            break;
+          case 9:
+            result += '-';
+            break;
+          default:
+            break;
+        }
+        result += value[j];
+      }
+      //
+      telephones[i].value = result;
+    });
+  }
+
+  const form = document.querySelector('.form');
+  const formErrorTel = form.querySelector('.form__error--tel');
+  const inputName = form.querySelector('#name');
+  const inputNameError = form.querySelector('.form__error--name');
+
+  form.addEventListener('submit', (evt) => {
+    if (telephones[0].value.length < 18) {
+      evt.preventDefault();
+      telephones[0].style.outline = '1px solid #ff5733';
+      telephones[0].style.boxShadow = 'inset 0 0 0 28px #ffe6e0';
+      formErrorTel.style.display = 'block';
+    }
+
+    if (inputName.value.length <= 1) {
+      evt.preventDefault();
+      inputName.style.outline = '1px solid #ff5733';
+      inputName.style.boxShadow = 'inset 0 0 0 28px #ffe6e0';
+      inputNameError.style.display = 'block';
+    }
+
+  });
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    // initSearch();
+
   });
 });
-
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✔️
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используейтся matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
